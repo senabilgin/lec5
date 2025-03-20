44411017c1034f8a75d741c075e614fa854f003c
senabilgin <49808970+senabilgin@users.noreply.github.com>
2025-03-20 14:07:54 -0400

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+
+        Vector3 position = transform.position;
+        position.x = 2;
+        transform.position = position;
+        Debug.Log("This is the first script.");
+
+        Vector3 scale = transform.localScale;
+        scale.x = 2;
+        scale.y = 2;
+        transform.localScale = scale;
+
+        rb2d.AddForce(new Vector2(10,10), ForceMode2D.Impulse);
+
+        //rb2d.AddForce(new Vector2(Random.Range()), ForceMode2D.Impulse);
+
+        
+    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
