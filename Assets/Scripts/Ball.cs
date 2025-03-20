18580dce9a27db1ac86c04345cb43beec06fed51
senabilgin <49808970+senabilgin@users.noreply.github.com>
2025-03-20 15:10:18 -0400

    task3
    added bang and the 20 count

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    private int countScore = 0;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+        
 
         Vector3 position = transform.position;
         position.x = 2;
@@ -17,12 +20,11 @@ public class Ball : MonoBehaviour
         scale.y = 2;
         transform.localScale = scale;
 
-        rb2d.AddForce(new Vector2(10,10), ForceMode2D.Impulse);
+        rb2d.AddForce(new Vector2(Random.Range(0, Mathf.Cos(1)), Random.Range(3, 5 )), ForceMode2D.Impulse);
 
         //rb2d.AddForce(new Vector2(Random.Range()), ForceMode2D.Impulse);
 
         
-    
 
     }
 
@@ -31,4 +33,20 @@ public class Ball : MonoBehaviour
     {
         
     }
+    
+
+     void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Bang");
+
+        
+        countScore++;
+
+        if (countScore > 20)
+        {
+            Debug.Log("Score is greater than 20!");
+        }
+    }
+    
+
 }
